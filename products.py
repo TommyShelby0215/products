263c0e2fdc095ad45a39f04248d7ae0c5ba40b4e
Tommy Wang <tommy860215@gmail.com>
2021-03-31 21:33:37 +0800

    update commit

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':#quick
 		break
 	price = input('請輸入商品價格: ')
+	price = int(price)
 	p = []#快寫法= p = {name, price}
 	p.append(name)
 	p.append(price)
@@ -17,3 +18,9 @@ for product in products:
 	print(product)
 	print(p[0])#每一個小清單的第0格
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')#因為前面將price轉化成整數而字串是不能跟整數+在一起，因此前面加個str()把整數轉化成字串
+		#\n是換行的意思
+		#csv檔可用excel開啟
